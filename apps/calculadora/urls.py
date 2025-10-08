c0cd3a359f7e18e306ef3e05328a9e58f88fa7be
Antonio1212122 <jantonioguerra055@gmail.com>
2025-10-08 10:11:49 -0600

    funciones basicas

--- apps/calculadora/urls.py
+++ apps/calculadora/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path("factorial/<int:numero>/", views.factorial_view, name="factorial"),
     path("potencia/<int:base>/<int:exponente>/", views.potencia_view, name="potencia"),
     path("raiz/<int:numero>/", views.raiz_view, name="raiz"),
+    path("suma/<str:num1>/<str:num2>/", views.suma_view, name="suma"),
+    path("resta/<str:num1>/<str:num2>/", views.resta_view, name="resta"),
+    path("multiplicacion/<str:num1>/<str:num2>/", views.multiplicacion_view, name="multiplicacion"),
+    path("division/<str:num1>/<str:num2>/", views.division_view, name="division"),
 ]
--- apps/calculadora/views.py
+++ apps/calculadora/views.py
@@ -24,3 +24,45 @@ def raiz_view(request, numero: float):
         return JsonResponse({"error": "No se puede calcular la raíz cuadrada de un número negativo."})
     resultado = math.sqrt(numero)
     return JsonResponse({"operacion": "raiz", "numero": numero, "resultado": resultado})
+
+# Función suma
+def suma_view(request, num1: str, num2: str):
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+    except ValueError:
+        return JsonResponse({"error": "Los valores deben ser numéricos."})
+    resultado = num1 + num2
+    return JsonResponse({"operacion": "suma", "num1": num1, "num2": num2, "resultado": resultado})
+
+# Función resta
+def resta_view(request, num1: str, num2: str):
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+    except ValueError:
+        return JsonResponse({"error": "Los valores deben ser numéricos."})
+    resultado = num1 - num2
+    return JsonResponse({"operacion": "resta", "num1": num1, "num2": num2, "resultado": resultado})
+
+# Función multiplicación
+def multiplicacion_view(request, num1: str, num2: str):
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+    except ValueError:
+        return JsonResponse({"error": "Los valores deben ser numéricos."})
+    resultado = num1 * num2
+    return JsonResponse({"operacion": "multiplicacion", "num1": num1, "num2": num2, "resultado": resultado})
+
+# Función división
+def division_view(request, num1: str, num2: str):
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+    except ValueError:
+        return JsonResponse({"error": "Los valores deben ser numéricos."})
+    if num2 == 0:
+        return JsonResponse({"error": "No se puede dividir por cero."})
+    resultado = num1 / num2
+    return JsonResponse({"operacion": "division", "num1": num1, "num2": num2, "resultado": resultado})
